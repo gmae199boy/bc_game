6bb4568075611208b7de01e8026125c268af4aba
gmae199boy <kim88594544@gmail.com>
2020-06-23 17:36:30 +0900

    ?

--- web_template/router/index.js
+++ web_template/router/index.js
@@ -1,23 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var passport = require('passport');
-var User = require('../model/user');
+var passport = require("passport");
+var User = require("../model/user");
 
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'hello', user: req.user });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "hello", user: req.user });
   // console.log(req.user.userId);
 });
 
-router.post('/', function(req, res){
+router.post("/", function (req, res) {
   var test = req.body.test;
   console.log(test);
-  res.render('index', {title: test, user: req.user});
+  res.render("index", { title: test, user: req.user });
 });
 
 module.exports = router;
